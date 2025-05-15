f7c7a5d3a4d1e66b40b58821896f84423fd02708
Thanapon Johdee <66236295+yokeTH@users.noreply.github.com>
2025-05-16 04:41:23 +0700

    feat: update Add Instructor href

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,7 +3,7 @@ const Navbar = () => {
     <div className="h-10 w-full bg-primary">
       <div className="container max-w-6xl flex h-full w-full mx-auto justify-end items-center">
         <a
-          href="https://github.com/CP-RektMart/jarn-nai/issues/new"
+          href="https://github.com/CP-RektMart/jarn-nai/issues/new?template=instructor-abbreviation.yml"
           className="text-white underline"
         >
           Add Instructor
