a57797e8de658137c9347fbc37a99b9dab2df50a
Thanapon Johdee <66236295+yokeTH@users.noreply.github.com>
2025-05-17 21:19:42 +0700

    fix: layout and consistance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,7 +61,7 @@ export default function InstructorSearch() {
         </AlertDescription>
       </Alert>
       <div className="text-left mb-4">
-        <h1 className="text-3xl font-bold mb-2 text-primary sm:m-2">
+        <h1 className="text-3xl font-bold mb-2 text-primary">
           CU Instructor Search
         </h1>
         {/* <p className="text-muted-foreground">Find instructor details by abbreviation, name, faculty, or department</p> */}
@@ -71,17 +71,17 @@ export default function InstructorSearch() {
         <div className="relative border-1 border-[#E0E0E0] has-[input:focus]:border-1 has-[input:focus]:border-[#2A2D48] hover:border-[#2A2D48] transition duration-100 rounded-sm box-border">
           <input
             placeholder="Search for instructors..."
-            className="rounded-sm py-2 pl-3.5 pr-10 border-0 w-full text-md sm:text-lg focus:ring-0 focus:ring-offset-0 focus:outline-none"
+            className="rounded-sm py-2 pl-3.5 pr-10 border-0 w-full text-md focus:ring-0 focus:ring-offset-0 focus:outline-none"
             value={searchTerm}
             onChange={(e) => handleSearch(e.target.value)}
           />
           <SearchIcon className="absolute right-3 top-1/2 -translate-y-1/2 h-5 w-5 text-muted-foreground" />
         </div>
       </div>
-      <p className="text-xs sm:text-sm text-muted-foreground mb-4 sm:m-3">
-        • Search by abbreviation, name, faculty or department <br />• Search
-        across all fields - abbreviation matches are shown first
-      </p>
+      <ul className="list-disc ml-4 text-xs text-muted-foreground mb-4">
+        <li>Search by abbreviation, name, faculty or department</li>
+        <li>Search across all fields - abbreviation matches are shown first</li>
+      </ul>
 
       <div className="space-y-4">
         {searched && results.length === 0 ? (
--- src/components/instructor-card.tsx
+++ src/components/instructor-card.tsx
@@ -19,9 +19,9 @@ const InstructorCard = ({
 }: InstructorCardProps) => {
   return (
     <Card key={abbreviation} className="overflow-hidden rounded-sm shadow-none">
-      <CardHeader className="pt-6 pb-0 px-4 sm:px-8">
+      <CardHeader className="pt-6 pb-0 px-4">
         <div className="flex flex-row justify-between items-start gap-2">
-          <CardTitle className="text-xl sm:text-2xl break-words">
+          <CardTitle className="text-xl break-words">
             {thFullName ?? enFullName}
           </CardTitle>
           <Badge variant="outline" className="font-mono py-1.5">
@@ -30,8 +30,8 @@ const InstructorCard = ({
         </div>
       </CardHeader>
 
-      <CardContent className="py-4 px-4 sm:px-8 p-l sm:w-2/3 lg:w-1/2">
-        <div className="grid gap-y-3 sm:grid-cols-3 sm:gap-x-4">
+      <CardContent className="py-4 px-4">
+        <div className="flex flex-wrap gap-4">
           <div className="flex items-start gap-2 flex-col ">
             <div className="flex items-start gap-2">
               <IdCard className="h-4 w-4 text-muted-foreground mt-1" />
@@ -46,43 +46,41 @@ const InstructorCard = ({
               {thFullName ?? enFullName}
             </Badge>
           </div>
-          <div className="flex flex-row gap-2 sm:contents">
+          <div className="flex flex-col items-start gap-2">
+            <div className="flex items-start gap-2">
+              <BuildingIcon className="h-4 w-4 text-muted-foreground mt-1" />
+              <span className="font-normal whitespace-nowrap">Faculty</span>
+            </div>
+            <Badge
+              variant="outline"
+              className={(() => {
+                switch (faculty.toLowerCase()) {
+                  case 'engineering':
+                    return 'font-normal border-[#941113] text-[#941113] break-words w-fit';
+                  default:
+                    return 'font-normal break-words w-fit';
+                }
+              })()}
+            >
+              {faculty}
+            </Badge>
+          </div>
+          {department && (
             <div className="flex flex-col items-start gap-2">
               <div className="flex items-start gap-2">
-                <BuildingIcon className="h-4 w-4 text-muted-foreground mt-1" />
-                <span className="font-normal whitespace-nowrap">Faculty</span>
+                <BookIcon className="h-4 w-4 text-muted-foreground mt-1" />
+                <span className="font-normal whitespace-nowrap">
+                  Department
+                </span>
               </div>
               <Badge
-                variant="outline"
-                className={(() => {
-                  switch (faculty.toLowerCase()) {
-                    case 'engineering':
-                      return 'font-normal border-[#941113] text-[#941113] break-words w-fit';
-                    default:
-                      return 'font-normal break-words w-fit';
-                  }
-                })()}
+                variant="secondary"
+                className="font-normal break-words w-fit"
               >
-                {faculty}
+                {department}
               </Badge>
             </div>
-            {department && (
-              <div className="flex flex-col items-start gap-2">
-                <div className="flex items-start gap-2">
-                  <BookIcon className="h-4 w-4 text-muted-foreground mt-1" />
-                  <span className="font-normal whitespace-nowrap">
-                    Department
-                  </span>
-                </div>
-                <Badge
-                  variant="secondary"
-                  className="font-normal break-words w-fit"
-                >
-                  {department}
-                </Badge>
-              </div>
-            )}
-          </div>
+          )}
         </div>
       </CardContent>
     </Card>
