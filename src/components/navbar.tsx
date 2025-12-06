5d94c3f2cfc8e1b4eeb7836191b01f7595a850c2
Thanapon Johdee <66236295+yokeTH@users.noreply.github.com>
2025-12-06 11:16:49 +0700

    fix: issue url

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,7 +3,7 @@ const Navbar = () => {
     <div className="h-10 w-full bg-primary">
       <div className="container mx-auto flex h-full w-full max-w-6xl items-center justify-end">
         <a
-          href="https://github.com/CP-RektMart/jarn-nai/issues/new?template=instructor-abbreviation.yml"
+          href="https://github.com/CP-RektMart/jarn-nai/issues/new?template=add-instructor-abbreviation.yml"
           className="mr-4 truncate text-white underline hover:text-gray-300"
         >
           Add Instructor
